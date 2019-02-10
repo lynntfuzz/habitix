d8f6fc323038d758ecffefd5fe894dd4a9efd600
DangerGreen <r.malikgreen@gmail.com>
2019-02-10 14:16:24 -0800

    finally got the navbar working; using 'reactstrap'

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
